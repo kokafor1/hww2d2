f3158b0bd65be70062c988382fac427c77db3ee2
kennedy <kennedyokafor2003@gmail.com>
2024-02-28 18:43:59 -0500

    i had to whip out my other computer that was stressful

--- hw.py
+++ hw.py
@@ -9,8 +9,11 @@ vowels = {'a', 'e', 'i', 'o', 'u'}
 for char in lowercase:
     if char in vowels:
         vowel += 1
+print(vowel)
 
 #Create a list of names of 4 letters or more and capitalize each name
 names = ['connor', 'connor', 'bob', 'connor', 'evan', 'max', 'evan', 'bob', 'kevin']        
 
 new_names = [name.capitalize() for name in names if len(name) >= 4]
+
+print(new_names)
