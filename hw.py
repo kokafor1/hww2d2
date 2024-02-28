2c125139e020a9a173da9d6f00f86276f90d7c69
kennedy <kennedyokafor2003@gmail.com>
2024-02-28 18:56:49 -0500

    i had to switch computers that was stressing me out

--- hw.py
+++ hw.py
@@ -1,19 +1,21 @@
 #Write a program that takes in a user input and prints out how many vowels are in the response
 
-users_input = input("Enter a phrase or sentence: ")
-vowel = 0
+# users_input = input("Enter a phrase or sentence: ")
+# vowel = 0
 
-lowercase = users_input.lower() #so it wont be case sensitive
-vowels = {'a', 'e', 'i', 'o', 'u'}
+# lowercase = users_input.lower() #so it wont be case sensitive
+# vowels = {'a', 'e', 'i', 'o', 'u'}
 
-for char in lowercase:
-    if char in vowels:
-        vowel += 1
-print(vowel)
+# for char in lowercase:
+#     if char in vowels:
+#         vowel += 1
+# # print(vowel)
 
 #Create a list of names of 4 letters or more and capitalize each name
-names = ['connor', 'connor', 'bob', 'connor', 'evan', 'max', 'evan', 'bob', 'kevin']        
+def user_names(names):
 
-new_names = [name.capitalize() for name in names if len(name) >= 4]
-
-print(new_names)
+    new_names = [name.capitalize() for name in names if len(name) >= 4]
+    return new_names
+names = ['connor', 'connor', 'bob', 'connor', 'evan', 'max', 'evan', 'bob', 'kevin']
+result = user_names(names)
+print(result)
